0fa90d66d4ebeabc8d39327058c4c5068392605b
jsssi <jeffesilva546@gmail.com>
2024-10-30 14:33:49 -0300

    feat: created register component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/login/login.component';
+import { RegisterComponent } from './modules/register/register.component';
 
 export const routes: Routes = [
 
   {
     path: "login",
     component: LoginComponent
+  },{
+    path: "register",
+    component: RegisterComponent
   },
   {
     path: '',
