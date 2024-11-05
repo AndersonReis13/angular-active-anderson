8d68fc6939a4f495d2145b1171ab378afec31758
Anderson Silva <andersonline62@gmail.com>
2024-11-05 09:33:46 -0300

    feat: creating forget passsword, fix: modificed login component and register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/login/login.component';
 import { RegisterComponent } from './modules/register/register.component';
+import { ForgetPasswordComponent } from './modules/forget-password/forget-password.component';
 
 export const routes: Routes = [
 
@@ -10,6 +11,9 @@ export const routes: Routes = [
   },{
     path: "register",
     component: RegisterComponent
+  },{
+    path: "forget",
+    component: ForgetPasswordComponent
   },
   {
     path: '',
